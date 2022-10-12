cdd123b8ff52c1572569da2a9b6d828c7b8f2985
Juan Mignaco <goleadornato@yahoo.com>
2022-10-12 16:02:29 -0300

    Add db and pg connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiWc2022Module } from './api-wc2022/api-wc2022.module';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [ApiWc2022Module],
+  imports: [
+    ApiWc2022Module,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [],
   providers: [],
 })
